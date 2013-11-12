05f33e529dec8ea75b397dbbd4f1db36837fa475
Dannon Baker <dannonbaker@me.com>
2013-11-12 14:10:19 -0800

    Disable htsql/django integration until we know we want it -- may do it by hand.

--- donorsdata/settings.py
+++ donorsdata/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'htsql_django',
+    #'htsql_django',
 )
 
 MIDDLEWARE_CLASSES = (
